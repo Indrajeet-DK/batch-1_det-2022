10a8ba02035c1ccc59f76279e7daf9f334483f69
Indrajeet-DK <103750526+Indrajeet-DK@users.noreply.github.com>
2022-08-30 18:05:53 +0530

    Add files via upload
    day 5 programs

---
+++ batch-1_det-2022/CSFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace batch_1_det_2022
+{
+    internal class CSFeature
+    {
+        public static void Main()
+        {
+            Action<string> action = (string name) => { Console.WriteLine($" hai{name}"); };
+            Action<string, string> action1 = (string s1, string s2) => { Console.WriteLine($" hai{s1}," + $"you said {s2}"); };
+
+            action.Invoke("mary");
+            action1.Invoke("mary", "am in blr");
+        }
+    }
+}
---
+++ batch-1_det-2022/ICar.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace batch_1_det_2022
+//{
+//    internal interface ICar
+//    {
+//        int GetPrice(string name);
+//    }
+
+//    class hyundai:ICar
+//    { public string Name { get; set; }
+
+//        public 
+
+//    }
+
+//    class honda:ICar
+//    {
+//        public string Name { get; set; }
+
+//    }
+
+//    class hyundai:ICar
+//    {
+//        public string Name { get; set; }
+
+//    }
+
+//    class BMW
+//    {
+//        public string Name { get; set; }
+
+//    }
+//}
+
